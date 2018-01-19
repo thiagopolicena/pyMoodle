f65e3585d4c9d19a25945dc20b4b97298879c20b
Marcelo Marcon <mmarconm@users.noreply.github.com>
2018-01-19 02:09:35 -0300

    Fixed de_or_created function
    Fixed del_or_create function was not deleting folder

--- analise_dados.py
+++ analise_dados.py
@@ -31,36 +31,40 @@ from os import path, makedirs
 import pandas as pd
 from bs4 import BeautifulSoup as bs
 
+
 BASE_DIR = path.dirname(path.abspath(__file__))
 del_data = path.join(BASE_DIR, 'del_data')
 gen_data = path.join(BASE_DIR, 'gen_data')
 rel_data = path.join(BASE_DIR, 'rel_data')
 
 folder_base = [
-path.join(BASE_DIR, 'rel_data'),
-path.join(BASE_DIR, 'rel_log'),
-path.join(BASE_DIR, 'clean_data')
+    path.join(BASE_DIR, 'rel_data'),
+    path.join(BASE_DIR, 'rel_log'),
+    path.join(BASE_DIR, 'clean_data')
 ]
 
-def del_create():
+
+def del_or_create():
     if os.path.isdir(gen_data):
-        shutil.rmtree(gen_data)
+        shutil.rmtree(gen_data, ignore_errors=True)
+        os.makedirs(gen_data)
     else:
-        makedirs('gen_data')
+        os.makedirs(gen_data)
 
     if os.path.isdir(del_data):
-        shutil.rmtree(del_data)
+        shutil.rmtree(del_data, ignore_errors=True)
+        os.makedirs(del_data)
     else:
-        makedirs('del_data')
+        os.makedirs(del_data)
 
     if os.path.isdir(rel_data):
-        shutil.rmtree(rel_data)
+        shutil.rmtree(rel_data, ignore_errors=True)
+        os.makedirs(rel_data)
     else:
-        makedirs('rel_data')
+        os.makedirs(rel_data)
 
 
 def get_dir():
-    del_create()
     # Getting the list of file in directory
     dired = []
     for item in glob('*'):
@@ -70,63 +74,68 @@ def get_dir():
     return dired
 
 
-f_open = get_dir()
+def create_log():
+    f_open = get_dir()
 
-for entry in f_open:
-    file = entry
-    file_data = path.join(del_data, 'del_{}'.format(file))
-    log_table = path.join(gen_data, 'gen_{}'.format(file))
-    log_pivot = path.join(rel_data, 'rel_{}'.format(file))
+    for entry in f_open:
+        file = entry
+        file_data = path.join(del_data, 'del_{}'.format(file))
+        log_table = path.join(gen_data, 'gen_{}'.format(file))
+        log_pivot = path.join(rel_data, 'rel_{}'.format(file))
 
-    with open(file, encoding='utf-8') as file_parse:
-        csv_reader = csv.reader(file_parse)
+        with open(file, encoding='utf-8') as file_parse:
+            csv_reader = csv.reader(file_parse)
 
-        with open(file_data, mode='w', newline='', encoding='mbcs') as new_file:
-            csv_writer = csv.writer(new_file)
+            with open(file_data, mode='w', newline='', encoding='mbcs') as new_file:
+                csv_writer = csv.writer(new_file)
 
-            csv_writer.writerow(('NOME', 'CONTEUDO'))
+                csv_writer.writerow(('NOME', 'CONTEUDO'))
 
-            next(csv_reader)
-            for line in csv_reader:
-                soup = bs(line[2], 'html.parser')
-                item = [u.string for u in soup.find_all('a')]
-                l = [u.upper() for u in item][0]
+                next(csv_reader)
+                for line in csv_reader:
+                    soup = bs(line[2], 'html.parser')
+                    item = [u.string for u in soup.find_all('a')]
+                    l = [u.upper() for u in item][0]
 
-                csv_writer.writerow([l, line[4].upper()])
-
-
-print('Log 1 Gerado com Sucesso')
-print('Gerando Proximo Log, Aguarde...')
+                    csv_writer.writerow([l, line[4].upper()])
 
 
-for entry in f_open:
-    file = entry
-    file_data = path.join(del_data, 'del_{}'.format(file))
-    log_table = path.join(gen_data, 'gen_{}'.format(file))
-    log_pivot = path.join(rel_data, 'rel_{}'.format(file))
-
-    with open(file_data, mode='r' ,encoding='mbcs') as pdf:
-        pd_file = pd.read_csv(pdf)
-        df = pd_file.groupby(['NOME','CONTEUDO'])['CONTEUDO'].count()
-
-        df.to_csv(log_table, sep=',', encoding='mbcs', header=True)
+    print('Log 1 Gerado com Sucesso')
+    print('Gerando Proximo Log, Aguarde...')
 
 
-print('Log 2 Gerado Com Sucesso')
-print('Gerando Proximo Log, Aguarde....')
+    for entry in f_open:
+        file = entry
+        file_data = path.join(del_data, 'del_{}'.format(file))
+        log_table = path.join(gen_data, 'gen_{}'.format(file))
+        log_pivot = path.join(rel_data, 'rel_{}'.format(file))
+
+        with open(file_data, mode='r' ,encoding='mbcs') as pdf:
+            pd_file = pd.read_csv(pdf)
+            df = pd_file.groupby(['NOME','CONTEUDO'])['CONTEUDO'].count()
+
+            df.to_csv(log_table, sep=',', encoding='mbcs', header=True)
 
 
-for entry in f_open:
-    file = entry
-    file_data = path.join(del_data, 'del_{}'.format(file))
-    log_table = path.join(gen_data, 'gen_{}'.format(file))
-    log_pivot = path.join(rel_data, 'rel_{}'.format(file))
-
-    with open(log_table, mode='r' ,encoding='mbcs') as pdf:
-        pivot = pd.read_csv(pdf)
-        df = pivot.pivot_table(columns=['CONTEUDO'], index=['NOME'])
-        df.to_csv(log_pivot, header=True, encoding='mbcs')
+    print('Log 2 Gerado Com Sucesso')
+    print('Gerando Proximo Log, Aguarde....')
 
 
-print('Arquivos Gerados com Sucesso, Verifique a pasta {}'.format(gen_data))
+    for entry in f_open:
+        file = entry
+        file_data = path.join(del_data, 'del_{}'.format(file))
+        log_table = path.join(gen_data, 'gen_{}'.format(file))
+        log_pivot = path.join(rel_data, 'rel_{}'.format(file))
 
+        with open(log_table, mode='r' ,encoding='mbcs') as pdf:
+            pivot = pd.read_csv(pdf)
+            df = pivot.pivot_table(columns=['CONTEUDO'], index=['NOME'])
+            df.to_csv(log_pivot, header=True, encoding='mbcs')
+
+
+    print('Arquivos Gerados com Sucesso, Verifique a pasta {}'.format(gen_data))
+
+
+if __name__ == '__main__':
+    del_or_create()
+    create_log()
