226c7e65c3602bd8b36d3dc6186efecd6bd5bae5
Marcelo Marcon <marcelomarcon@outlook.com>
2019-08-27 15:02:07 -0400

    modificado para fazer a geracao de log para moodle 2019

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ PyMoodle
 
 ## Authors
 
-* **Marcelo Marcon** - *Initial work* - [mmarconm](https://github.com/mmarconm/pyMoodle)
+- **Marcelo Marcon** - _Initial work_ - [mmarconm](https://github.com/mmarconm/pyMoodle)
 
 ## Getting Started
 
@@ -13,20 +13,22 @@ abra o terminal e digite.
 
 ```
 pip install -U --user pip
-pip install -U --user beautifulsoup4 pandas
 ```
 
 caso voce instale o python global para todos os usuarios voce tera que usar o terminal
 com permissao de administrador.
 Python 3 para Windows
+
 ```
 https://www.python.org/ftp/python/3.6.5/python-3.6.5-amd64-webinstall.exe
 ```
 
 Python 3 para Linux
+
 ```
 sudo apt install python3 python3-pip python3-virtualenv
 ```
+
 ### Prerequisites
 
 Muito importante o arquivo requirements.txt contem as bibliotecas que deveram ser instaladas, para instalar use o pip com a flag -r
@@ -51,15 +53,13 @@ python analise_dados.py
 
 Dependendo da quantidade de arquivo, tamanho e velocidade do seu processador isso pode levar alguns minutos, ele irá criar algumas pastas com os logs separados.
 
-
 ## Deployment
 
 Author: Marcelo Marcon - marconm.inf0@gmail.com
 
 ## Built With
 
-* [Marcelo Marcon](https://github.com/mmarconm/pyMoodle) - Python Dev
-
+- [Marcelo Marcon](https://github.com/mmarconm/pyMoodle) - Python Dev
 
 ## License
 
@@ -67,5 +67,5 @@ This project is licensed under the MIT License - see the [LICENSE](LICENSE) file
 
 ## Acknowledgments
 
-* Moodle
-* Python Devs
+- Moodle
+- Python Devs
--- analise_moodle_2019.py
+++ analise_moodle_2019.py
@@ -1,6 +1,7 @@
+#!/usr/bin/env python3
 """
 Author: Marcelo Marcon - marconm.inf0@gmail.com
-Versao 1.0.1 ### 01/2018
+Versao 1.0.2 ### 08/2019
 Gerador de Log para o portal Moodle
 O log ira preservar apenas duas colunas, Como Nome e Conteudo
 ################################################################
@@ -10,7 +11,6 @@ bs4 ou beautifulsoup4 e pandas estao instalados na maquina.
 use o pip package manager do python para instalar.
 abra o terminal e digite >>>
 pip install -U --user pip
-pip install -U --user beautifulsoup4 pandas
 caso voce instale o python global para todos os usuarios voce tera que usar o terminal
 com permissao de administrador.
 
@@ -19,7 +19,9 @@ coloque o arquivo dentro da pasta que tenha todos os arquivos csv ou txt
 python analise_dados.py e aguarde alguns minutos
 
 """
-# ^[ \t\v]*#.*?coding[:=][ \t]*([-_.a-zA-Z0-9]+)
+# -*- coding: utf-8 -*-
+__author__ = 'mmaconm'
+#
 
 import csv
 import os.path
@@ -28,59 +30,38 @@ from glob import glob
 from os import path, name
 
 import pandas as pd
-from bs4 import BeautifulSoup as bs
 
-# Directory Support
-BASE_DIR = path.dirname(path.abspath(__file__))
+BASE_DIR = path.abspath(path.dirname(__file__))
 del_data = path.join(BASE_DIR, 'del_data')
 gen_data = path.join(BASE_DIR, 'gen_data')
 rel_data = path.join(BASE_DIR, 'rel_data')
 
-folder_base = [
-    path.join(BASE_DIR, 'rel_data'),
-    path.join(BASE_DIR, 'rel_log'),
-    path.join(BASE_DIR, 'clean_data')
-]
 
+def get_os() -> str:
+    # retorna o tipo de encoding
+    # baseado no sitema operacional
 
-def get_os():
-    os_system = None
     if 'posix' in os.name:
-        os_system = 'utf-8'
-        return os_system
-    else:
-        os_system = 'mbcs'
-        return os_system
+        return 'latin1'  # linux
+
+    return 'mbcs'  # windows
 
 
 def del_or_create():
-    if os.path.isdir(gen_data):
-        shutil.rmtree(gen_data, ignore_errors=True)
-        os.makedirs(gen_data)
-    else:
-        os.makedirs(gen_data)
-
-    if os.path.isdir(del_data):
-        shutil.rmtree(del_data, ignore_errors=True)
-        os.makedirs(del_data)
-    else:
-        os.makedirs(del_data)
-
-    if os.path.isdir(rel_data):
-        shutil.rmtree(rel_data, ignore_errors=True)
-        os.makedirs(rel_data)
-    else:
-        os.makedirs(rel_data)
+    # verifica as pastas se existem e deleta a mesma e a recria de novo
+    for folder in [del_data, gen_data, rel_data]:
+        if os.path.isdir(folder):
+            shutil.rmtree(folder, ignore_errors=True)
+            os.makedirs(folder)
+        else:
+            # caso nao exista as pastas, apenas cria
+            os.makedirs(folder)
 
 
-def get_dir():
-    # Getting the list of file in directory
-    dired = []
-    for item in glob('*'):
-        if item.endswith('.csv'):
-            dired.append(item)
-
-    return dired
+def get_dir(ftype: str = 'csv') -> list:
+    # lista todos os csv's no diretorio atual
+    # vai retornar uma lista
+    return [i for i in glob('*') if i.endswith(f'.{ftype}')] or None
 
 
 def create_log():
@@ -104,9 +85,7 @@ def create_log():
 
                 next(csv_reader)
                 for line in csv_reader:
-                    soup = bs(line[2], 'html.parser')
-                    item = [u.string for u in soup.find_all('a')]
-                    l = [u.upper() for u in item][0]
+                    l = [u.upper() for u in line][1]
 
                     csv_writer.writerow([l, line[4].upper()])
 
@@ -125,9 +104,6 @@ def create_log():
 
             df.to_csv(log_table, sep=',', encoding=get_os(), header=True)
 
-    print('Log 2 Gerado Com Sucesso')
-    print('Gerando Proximo Log, Aguarde....')
-
     for entry in f_open:
         file = entry
         file_data = path.join(del_data, 'del_{}'.format(file))
@@ -139,6 +115,7 @@ def create_log():
             df = pivot.pivot_table(columns=['CONTEUDO'], index=['NOME'])
             df.to_csv(log_pivot, header=True, encoding=get_os())
 
+    print('Logs para Moodle 2019/1 Terminados')
     print('Arquivos Gerados com Sucesso, Verifique a pasta {}'.format(rel_data))
 
 
